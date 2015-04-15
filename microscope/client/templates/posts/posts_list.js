6490b86cc5a349d7abbd318cc4f61c3d43e6824e
Radizzt <Richardthienhuynh@gmail.com>
2015-04-15 13:57:38 -0400

    Wired collection into 'postsList' template

--- microscope/client/templates/posts/posts_list.js
+++ microscope/client/templates/posts/posts_list.js
@@ -1,20 +1,9 @@
 /**
  * Created by MAC1 on 2015-04-14.
+ * Pulling data from the database and returning the value to postData
  */
-var postsData = [
-    {
-        title: 'Introducing Telescope',
-        url: 'http://sachagreif.com/introducing-telescope/'
-    },
-    {
-        title: 'Meteor',
-        url: 'http://meteor.com'
-    },
-    {
-        title: 'The Meteor Book',
-        url: 'http://themeteorbook.com'
-    }
-];
 Template.postsList.helpers({
-    posts: postsData
+    posts: function() {
+        return Posts.find();
+    }
 });
