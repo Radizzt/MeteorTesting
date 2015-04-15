d580b7d8480d4183321ee143197a0861bf89cabd
Radizzt <Richardthienhuynh@gmail.com>
2015-04-15 17:57:25 -0400

    Wait on the post subscription.

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -1,8 +1,12 @@
 /**
  * Created by Huynh on 4/15/2015.
  */
+/*In the client, we need to subscribe to the publication
+ * Name must be the same as the one in /server/publication.js */
 Router.configure({
-    layoutTemplate: 'layout'
+    layoutTemplate: 'layout',
+    loadingTemplate: 'loading',
+    waitOn: function() { return Meteor.subscribe('posts');}
 });
 //Name of the route will look for a template with the same name - postsList in this case
 Router.route('/', {name: 'postsList'});
