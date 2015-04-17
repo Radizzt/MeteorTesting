fd3c3a336119a9d1737f51a1b3136ff92cf8778c
Radizzt <Richardthienhuynh@gmail.com>
2015-04-17 15:58:25 -0400

    Validate post contents when editing.

--- microscope/client/templates/posts/post_edit.js
+++ microscope/client/templates/posts/post_edit.js
@@ -1,6 +1,18 @@
 /**
  * Created by MAC1 on 2015-04-17.
  */
+Template.postEdit.created = function() {
+    Session.set('postEditErrors', {});
+}
+Template.postEdit.helpers({
+    errorMessage: function(field) {
+        return Session.get('postEditErrors')[field];
+    },
+    errorClass: function (field) {
+        return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+    }
+});
+
 Template.postEdit.events({
     'submit form': function(e) {
         e.preventDefault();
@@ -17,9 +29,13 @@ Template.postEdit.events({
         }
         else
         {
+            var errors = validatePost(postProperties);
+            if (errors.title || errors.url)
+                return Session.set('postEditErrors', errors);
+
             Posts.update(currentPostId, {$set: postProperties}, function(error) {
                 if (error) {
-// display the error to the user
+                    // display the error to the user
                     throwError(error.reason);
                 } else {
                     Router.go('postPage', {_id: currentPostId});
