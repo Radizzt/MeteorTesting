5e32f1c12f46a802004010a8176163f44cc7baf1
Radizzt <Richardthienhuynh@gmail.com>
2015-04-21 17:37:32 -0400

    13-1 Added basic upvoting algorithm.

--- microscope/server/fixtures.js
+++ microscope/server/fixtures.js
@@ -19,7 +19,9 @@ if (Posts.find().count() === 0) {
         author: sacha.profile.name,
         url: 'http://sachagreif.com/introducing-telescope/',
         submitted: new Date(now - 7 * 3600 * 1000),
-        commentsCount: 2
+        commentsCount: 2,
+        upvoters: [],
+        votes: 0
     });
     Comments.insert({
         postId: telescopeId,
@@ -36,12 +38,24 @@ if (Posts.find().count() === 0) {
         body: 'You sure can Tom!'
     });
     Posts.insert({
+        title: 'Meteor',
+        userId: tom._id,
+        author: tom.profile.name,
+        url: 'http://meteor.com',
+        submitted: new Date(now - 10 * 3600 * 1000),
+        commentsCount: 0,
+        upvoters: [],
+        votes: 0
+    });
+    Posts.insert({
         title: 'The Meteor Book',
         userId: tom._id,
         author: tom.profile.name,
         url: 'http://themeteorbook.com',
         submitted: new Date(now - 12 * 3600 * 1000),
-        commentsCount: 0
+        commentsCount: 0,
+        upvoters: [],
+        votes: 0
     });
     for (var i = 0; i < 10; i++) {
         Posts.insert({
@@ -49,8 +63,10 @@ if (Posts.find().count() === 0) {
             author: sacha.profile.name,
             userId: sacha._id,
             url: 'http://google.com/?q=test-' + i,
-            submitted: new Date(now - i * 3600 * 1000),
-            commentsCount: 0
+            submitted: new Date(now - i * 3600 * 1000 + 1),
+            commentsCount: 0,
+            upvoters: [],
+            votes: 0
         });
     }
 }
