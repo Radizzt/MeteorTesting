747efb2996c177ab6d23c32e91f574a393f078ff
Radizzt <Richardthienhuynh@gmail.com>
2015-04-23 15:22:56 -0400

    16-1, Added http package and created Bitly package.

---
+++ microscope/packages/bitly/package.js
@@ -0,0 +1,14 @@
+/**
+ * Created by MAC1 on 2015-04-23.
+ */
+Package.describe({
+    name: 'bitly',
+    summary: "Bitly package",
+    version: '1.0.0'
+});
+
+Package.onUse(function (api) {
+    api.versionsFrom('0.9.4');
+    api.addFiles('bitly.js', 'server');
+    api.export('Bitly');
+});
