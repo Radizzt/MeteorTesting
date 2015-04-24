38ad1c4f1f4de7051258c752dfb38e631516d538
Radizzt <Richardthienhuynh@gmail.com>
2015-04-24 11:11:36 -0400

    17-1, Added Intercom code snippet.

---
+++ microscope/client/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by MAC1 on 2015-04-24.
+ */
+window.intercomSettings = {
+    // TODO: The current logged in user's full name
+    name: "John Doe",
+    // TODO: The current logged in user's email address.
+    email: "john.doe@example.com",
+    // TODO: The current logged in user's sign-up date as a Unix timestamp.
+    created_at: 1234567890,
+    app_id: "qgzveb1w"
+};
